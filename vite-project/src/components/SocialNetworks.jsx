d759bc297f78b31028411b258906c21999f31fee
elton lima <eltonathayde2@gmail.com>
2022-08-17 15:25:01 -0300

    svg das socialnetwork addiconads ao projeto

--- vite-project/src/components/SocialNetworks.jsx
+++ vite-project/src/components/SocialNetworks.jsx
@@ -2,8 +2,21 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
 
 import "../styles/components/socialnetworks.sass";
 
+const socialnetworks = [
+  { name: "linkedin", icon: <FaLinkedinIn /> },
+  { name: "github", icon: <FaGithub /> },
+  { name: "instagram", icon: <FaInstagram /> },
+];
 const SocialNetworks = () => {
-  return <div>SocialNetworks</div>;
+  return (
+    <section id="social-network">
+      {socialnetworks.map((network) => (
+        <a href="#" className="social-btn" id={network.name} key={network.name}>
+          {network.icon}
+        </a>
+      ))}
+    </section>
+  );
 };
 
 export default SocialNetworks;
