32d7d83afe0ec3604bee715eebf453bd17ceff75
elton lima <eltonathayde2@gmail.com>
2022-08-19 14:55:24 -0300

    projeto 95% finalizado - falta adicionar info-pessoais

---
+++ vite-project/src/components/AboutContainer.jsx
@@ -0,0 +1,21 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo odit
+        provident sunt distinctio ab rerum non? Nostrum cupiditate consequatur
+        quod obcaecati praesentium, at illo delectus velit atque aut aliquid
+        rerum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+        tempora, expedita assumenda repellat corporis voluptas eaque similique
+        illo quos, vero repudiandae doloribus totam neque optio cum, eum magnam
+        libero voluptate!
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ vite-project/src/components/ProjectContainer.jsx
@@ -0,0 +1,18 @@
+const ProjectContainer = () => {
+  return (
+    <section className="porjects-container">
+      <h2>projetos</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+        accusamus aliquam asperiores ipsum. Voluptatem consequuntur culpa
+        repellendus aliquid, magni enim cupiditate, vel sunt adipisci minima
+        nobis? Quo eum maiores reprehenderit.
+      </p>
+      <a href="https://github.com/eltonathayde" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectContainer;
